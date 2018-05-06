dfcc22fb9d5e0e1a652efb88b027ba4508f6d19f
sumnernh@gmail.com <sumnernh@gmail.com>
2018-05-06 14:12:08 -0700

    Started work on RNN brain

---
+++ LSTMBrain.py
@@ -0,0 +1,214 @@
+from brain import Brain
+import tensorflow as tf
+import os
+
+
+class LSTMBrain(Brain):
+    DUALCOPYFREQ = 1000
+
+    SESS = None
+    WRITER = None
+    MERGER = None
+    SESS_HOLDERS = 0
+
+    # https://stats.stackexchange.com/questions/200006/q-learning-with-neural-network-as-function-approximation/200146
+    # https://stats.stackexchange.com/questions/126994/questions-about-q-learning-using-neural-networks
+    def __init__(self, name, ninputs, nactions, hshapes=list((25,10)), gamma=0.9, directory="save", rewardbuffer=None):
+        # Make a single session if not inherited
+        super().__init__(name, ninputs, nactions,
+                         directory=directory, rewardbuffer=rewardbuffer)
+        self.inherited_sess = False
+
+        self.name = name
+        self.directory = directory
+
+        if LSTMBrain.SESS_HOLDERS == 0:
+            tf.reset_default_graph()
+        LSTMBrain.SESS_HOLDERS += 1
+
+        # Put all vars into name scope
+        with tf.name_scope(name):
+            mainshape = [ninputs]+hshapes+[nactions]
+
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+
+            # Make some models with input/output variables
+            self.state_in, self.Qout, self.qnetvars = self.makeqnetwork(mainshape)
+            self.next_state, self.dualQout, self.dualnetvars = self.makeqnetwork(mainshape)
+
+            # Copy each var to dual
+            self.copy_to_dual = [tf.assign(dualnetvar, qnetvar) for qnetvar, dualnetvar in
+                                 zip(self.qnetvars, self.dualnetvars)]
+
+            # Q probs
+            self.qprobs = tf.nn.softmax(self.Qout, 1)
+            # Take random sample from each and put into array
+            self.prob_chosen_actions = tf.reshape(tf.multinomial(tf.log(self.qprobs), 1), [-1])
+
+            # Then combine them together to get our final Q-values.
+            self.prob_chosen_Q = tf.reduce_sum(tf.multiply(self.Qout,
+                                                           tf.one_hot(self.prob_chosen_actions, nactions,
+                                                                      dtype=tf.float32)),
+                                               axis=1)
+
+            # If we want just the highest Q value do the following
+            self.chosen_actions = tf.argmax(self.Qout, 1)
+
+            # Then combine them together to get our final Q-values.
+            self.chosen_Q = tf.reduce_sum(tf.multiply(self.Qout,
+                                                      tf.one_hot(self.chosen_actions, nactions, dtype=tf.float32)),
+                                          axis=1)
+
+            # Below we obtain the loss by taking the sum of squares difference
+            # between the target and prediction Q values.
+            self.rewards = tf.placeholder(shape=[None], dtype=tf.float32)
+            self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+
+            self.actions_onehot = tf.one_hot(self.actions, nactions, dtype=tf.float32)
+
+            # Q of chosen actions
+            self.Q = tf.reduce_sum(tf.multiply(self.Qout, self.actions_onehot), axis=1)
+
+            # Q(s,a) = R + y * max_a'( Q(s',a') )
+            self.targetQ = self.rewards + gamma * tf.reduce_max(self.dualQout, 1)
+
+            self.td_error = tf.square(self.targetQ - self.Q)
+            self.loss = tf.reduce_mean(self.td_error)
+            self.trainer = tf.train.AdamOptimizer(learning_rate=0.0002)
+            self.updateModel = self.trainer.minimize(self.loss, global_step=self.global_step)
+
+            self.saver = tf.train.Saver()
+            with tf.name_scope('summary'):
+                self.losssum = tf.summary.scalar('loss', self.loss)
+
+                self.rewardsumvar = tf.placeholder(shape=(), name="episode_reward", dtype=tf.float32)
+                self.rewardsum = tf.summary.scalar('reward', self.rewardsumvar)
+
+    def makeqnetwork(self, shape, valueshape=None, advantageshape=None):
+        """
+        Construct graph
+        :param inputshape:
+        :param noinit:
+        :return: input placeholder, output layer, list of variables
+        """
+        # Build brain model
+        state_in = tf.placeholder(shape=[None, shape[0]], dtype=tf.float32)
+        layer = state_in
+        variables = []
+
+        print(shape)
+
+        for i in range(0, len(shape)-2):
+            W = tf.Variable(tf.random_normal([shape[i],shape[i+1]]))
+            b = tf.Variable(tf.random_normal([shape[i+1]]))
+            layer = tf.nn.relu(tf.add(tf.matmul(layer,W),b))
+            variables.append(W)
+            variables.append(b)
+
+        # Make output layer without relu
+        W = tf.Variable(tf.random_normal([shape[-2],shape[-1]]))
+        b = tf.Variable(tf.random_normal([shape[-1]]))
+        layer = tf.add(tf.matmul(layer, W), b)
+        variables.append(W)
+        variables.append(b)
+
+        return state_in, layer, variables
+
+    def get_checkpoint(self):
+        if not os.path.isdir(self.directory):
+            os.mkdir(self.directory)
+        return os.path.join(self.directory, self.name)
+
+    def has_checkpoint(self):
+        return tf.train.checkpoint_exists(self.get_checkpoint())
+
+    def loadcheckpoint(self):
+        if self.has_checkpoint():
+            print("Loading checkpoint... ",end="")
+            self.saver.restore(TFBrain.SESS, self.get_checkpoint())
+            print("Done!")
+        else:
+            print("No checkpoint found")
+
+    def think(self, inputs):
+        if len(inputs) == 0:
+            return {}
+
+        ids = list(inputs.keys())
+        # Here we can choose either prob_chosen_actions or chosen_actions
+        acts = TFBrain.SESS.run(self.prob_chosen_actions,
+                                feed_dict={self.state_in: [inputs[entityid] for entityid in ids]})
+        return {entityid: act for entityid, act in zip(ids, acts)}
+
+    def debug(self,debuginput):
+        actprobs = TFBrain.SESS.run(self.qprobs, feed_dict={self.state_in: [debuginput]})
+        return actprobs[0]
+
+    def train(self, iters, batch, totreward=None):
+        """
+        Train the brain for a bit based in rewards previously provided
+        :param niters: number of training iterations
+        :param batch: batch size
+        :return:
+        """
+        if totreward is not None:
+            rewardsum, global_step = TFBrain.SESS.run([self.rewardsum, self.global_step],
+                                                      feed_dict={self.rewardsumvar: totreward})
+            TFBrain.WRITER.add_summary(rewardsum, global_step)
+
+        print("Buffer size: {}".format(len(self.buffer)))
+        print("Gen niters: {}".format(int(TFBrain.DUALCOPYFREQ)))
+        print("\tncopies: {}".format(int(iters/TFBrain.DUALCOPYFREQ)))
+        for i in range(int(iters/TFBrain.DUALCOPYFREQ)):
+            training_gen = self.buffer.get_batch_gen(batchsize=batch, niters=int(TFBrain.DUALCOPYFREQ))
+            self.trainbatch(training_gen)
+
+    def trainbatch(self, gen):
+        # Make dual graph identical to primary
+        TFBrain.SESS.run(self.copy_to_dual)
+
+        # Train primary
+        for inputs, actions, rewards, newinputs in gen:
+            feed_dict = {self.state_in: inputs,
+                         self.rewards: rewards,
+                         self.actions: actions,
+                         self.next_state: newinputs}
+            _, summary, global_step = TFBrain.SESS.run([self.updateModel, self.losssum, self.global_step],
+                                                       feed_dict=feed_dict)
+            TFBrain.WRITER.add_summary(summary, global_step)
+
+    def startup(self):
+        super().startup()
+        if TFBrain.SESS is None:
+            TFBrain.SESS = tf.Session()
+            # Logs
+            TFBrain.WRITER = tf.summary.FileWriter("output", TFBrain.SESS.graph)
+
+            init = tf.global_variables_initializer()
+            TFBrain.SESS.run(init)
+        self.loadcheckpoint()
+
+    def save(self):
+        """
+        Saves variables to directory
+        """
+        print("Saving {}... ".format(self.name), end='')
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+        self.saver.save(TFBrain.SESS, self.get_checkpoint())
+        print("Done!")
+
+    def cleanup(self):
+        super().cleanup()
+        self.save()
+        TFBrain.SESS_HOLDERS -= 1
+        if TFBrain.SESS_HOLDERS == 0:
+            TFBrain.SESS.close()
+            TFBrain.WRITER.close()
+
+    def print_diag(self, sample_in):
+        pass
+
+
+def formatarray(array):
+    return " ".join("{:5.5f}".format(f) for f in array)
---
+++ historybuffer.py
@@ -0,0 +1,98 @@
+import numpy
+import os
+
+
+class HistoryRewardBuffer:
+    def __init__(self, name, inputsize,
+                 directory='save',buffersize=100000, historylen=100):
+        """
+        :param buffersize:
+        """
+        self.states = numpy.ndarray(shape=(buffersize,inputsize), dtype=float)
+        self.actions = numpy.ndarray(shape=(buffersize,), dtype=int)
+        self.rewards = numpy.ndarray(shape=(buffersize,), dtype=float)
+        self.nextstates = numpy.ndarray(shape=(buffersize,inputsize), dtype=float)
+        self.buffersize = buffersize
+        self.size = 0
+        self.head = 0
+        self.name = name
+        self.directory = directory
+        self.dirty = False
+
+    def reward(self,inputs,actions,rewards,newinputs):
+        """
+        Provide dicts of {id:item}
+        :param inputs:
+        :param actions:
+        :param rewards:
+        :param newinputs:
+        """
+        for entityid in inputs.keys():
+            entityin, entityact = inputs[entityid], actions[entityid]
+            entityrew, entitynewin = rewards[entityid], newinputs[entityid]
+
+            self.states[self.head, :] = entityin
+            self.actions[self.head] = entityact
+            self.rewards[self.head] = entityrew
+            self.nextstates[self.head, :] = entitynewin
+
+            self.head = (self.head + 1) % self.buffersize
+            self.size = min(self.size+1, self.buffersize)
+        self.dirty = True
+
+    def get_batch_gen(self,batchsize,niters):
+        """
+        Make a generator which provides batches of items
+        :param batchsize: size of batch
+        :param niters: number of batches to produce
+        :return:
+        """
+        # Array of all (input, action, reward)
+        def gen():
+            # Choose and yield sets of results
+            for i in range(niters):
+                choices = numpy.random.choice(self.size,batchsize)
+                yield self.states[choices], self.actions[choices], self.rewards[choices], self.nextstates[choices]
+        return gen()
+
+    def clear(self):
+        self.size = 0
+        self.head = 0
+        self.dirty = True
+
+    def save(self):
+        if self.dirty:
+            print("Saving buffer... ",end='')
+            substates = self.states[:self.size]
+            subactions = self.actions[:self.size]
+            subrewards = self.rewards[:self.size]
+            subnext = self.nextstates[:self.size]
+            numpy.savez_compressed(os.path.join(self.directory, self.name),
+                                   states=substates, actions=subactions,
+                                   rewards=subrewards, nexts=subnext)
+            print("Done!")
+            self.dirty = False
+
+    def load(self):
+        savename = os.path.join(self.directory, self.name if self.name.endswith('.npz') else self.name + '.npz')
+        if os.path.exists(savename) and not self.dirty:
+            print("Loading buffer... ", end='')
+
+            loaded = numpy.load(savename)
+
+            substates = loaded['states']
+            subactions = loaded['actions']
+            subrewards = loaded['rewards']
+            subnext = loaded['nexts']
+
+            self.states[:len(substates)] = substates
+            self.actions[:len(subactions)] = subactions
+            self.rewards[:len(subrewards)] = subrewards
+            self.nextstates[:len(subnext)] = subnext
+            self.size = len(substates)
+            self.head = self.size-1
+            self.dirty = True
+            print("Done!")
+
+    def __len__(self):
+        return self.size
